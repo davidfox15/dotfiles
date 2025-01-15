8787f36d5c4cdf8c04c6854a60d335bfed3e4791
David Lisitsyn <davidfox15@yandex.ru>
2025-01-15 10:28:04 +0300

    improves

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font_size = 16
-config.font = wezterm.font("JetBrainsMonoNL Nerd Font Mono")
+config.font = wezterm.font_with_fallback({ "JetBrainsMonoNL Nerd Font Mono", "Symbols Nerd Font Mono" })
 -- config.font = wezterm.font("JetBrains Mono")
 
 config.enable_tab_bar = false
@@ -16,6 +16,13 @@ config.window_decorations = "RESIZE"
 -- 	cursor_bg = "#f1e0dc",
 -- 	cursor_fg = "#1e1d2c",
 -- }
+
+config.unix_domains = {
+	{
+		name = "unix",
+		local_echo_threshold_ms = 10,
+	},
+}
 
 config.color_scheme = "Catppuccin Mocha"
 
